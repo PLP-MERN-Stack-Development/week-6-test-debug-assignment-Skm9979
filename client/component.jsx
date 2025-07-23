067548f07136707acf11519134dc1f78dded5d53
SOFIA MOHAMMED <keasofia236@gmail.com>
2025-07-23 18:58:56 +0300

    Create component.jsx

---
+++ client/component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ExampleComponent = () => {
+  return (
+    <div data-testid="example-component">
+      <h1>Example Component</h1>
+      <p>This is a sample component for testing</p>
+    </div>
+  );
+};
+
+export default ExampleComponent;
